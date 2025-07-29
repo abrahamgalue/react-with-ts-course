2494d369d583695bc0fbb63a6852a6403ac29e00
Abraham Galue <abrahamgalue512@gmail.com>
2025-07-29 16:09:47 -0400

    State with more complex types

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,31 @@ import { useState, useEffect } from "react";
 import RandomFox from "@/app/components/RandomFox";
 
 const random = () => Math.floor(Math.random() * 123) + 1;
+const generateId = () => crypto.randomUUID();
+
+type ImageItem = { id: string; url: string };
 
 export default function Home() {
-  const [images, setImages] = useState<string[]>([]);
+  const [images, setImages] = useState<ImageItem[]>([]);
 
   useEffect(() => {
     setImages([
-      `http://randomfox.ca/images/${random()}.jpg`,
-      `http://randomfox.ca/images/${random()}.jpg`,
-      `http://randomfox.ca/images/${random()}.jpg`,
-      `http://randomfox.ca/images/${random()}.jpg`,
+      {
+        id: generateId(),
+        url: `http://randomfox.ca/images/${random()}.jpg`,
+      },
+      {
+        id: generateId(),
+        url: `http://randomfox.ca/images/${random()}.jpg`,
+      },
+      {
+        id: generateId(),
+        url: `http://randomfox.ca/images/${random()}.jpg`,
+      },
+      {
+        id: generateId(),
+        url: `http://randomfox.ca/images/${random()}.jpg`,
+      },
     ]);
   }, []);
 
@@ -20,9 +35,9 @@ export default function Home() {
     <div>
       <main>
         <h1 className="text-3xl font-bold underline">Hello React with TS</h1>
-        {images.map((image, index) => (
-          <div key={index} className="p-4">
-            <RandomFox image={image} />
+        {images.map(({ id, url }) => (
+          <div key={id} className="p-4">
+            <RandomFox image={url} />
           </div>
         ))}
       </main>
