7634524ea5aaff4389dd340c963aa741955c70d7
Abraham Galue <abrahamgalue512@gmail.com>
2025-07-29 16:55:17 -0400

    Lazy loading with IntersectionObserver

--- app/components/RandomFox.tsx
+++ app/components/RandomFox.tsx
@@ -1,20 +1,45 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 type Props = {
   image: string;
 };
 
 const RandomFox = ({ image }: Props) => {
+  const [src, setSrc] = useState(
+    "https://signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png"
+  );
   const node = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    // new observer
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        // onIntersection
+        if (entry.isIntersecting) {
+          setSrc(image);
+        }
+      });
+    });
+
+    // observe node
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    // disconnect
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
 
   return (
     <picture>
       <img
         alt="random fox image"
-        className="rounded"
+        className="rounded bg-gray-300"
         height="auto"
         ref={node}
-        src={image}
+        src={src}
         width={320}
       />
     </picture>
